a1ee2fb600289b5b99afd5ec633aa3c44d3b13bc
Shakhrizoda07 <shakhrizoda752@gmail.com>
2025-01-28 04:14:18 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// let i = 0;
+// while(i < 3){
+//     console.log(i)
+//     i++
+// }   // iteratsiyadan oldin tekshiradi
+
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(0);  //iteratsiyadan keyn tekshiradi
+
+
+// for (i = 0; i < 10; i++){
+//     if(i == 7)break;  //kodi toxtadi
+//     console.log(i)
+// }   
+
+// let sum = 0;
+// while(true){
+//     let value = +prompt('Введите число');
+//     if (!value) break;
+//     sum += value;
+// }
+// console.log('Сумма:' + sum);
+
+// for (i = 0; i <= 100; i+=){
+//     console.log(i) 
+// }
+
+// let number = ['2', '4', '9', '10', '45', '67', '8', '90']
+// console.log(number);
+
+// for (number = 0; number <=2,4,9,10,45,67,8,90; number+=){
+//     console.log(number)
+// };
+
+
+
+let arr = [2, 4, 9, 10, 45, 67, 8, 90];
+let evenNumbers = arr.filter(num => num % 2 === 0);
+console.log(arr,); 
+console.log(evenNumbers,);
+
